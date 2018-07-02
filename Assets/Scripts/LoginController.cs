da93de7f0d81ecbacd86167bc961c60b9fd6c4a5
ade <ade.li.indie@gmail.com>
2018-07-02 19:20:27 +0800

    init

---
+++ Assets/Scripts/LoginController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using netco;
+using System.Net;
+
+public class LoginController : MonoBehaviour {
+    public Text networkState;
+    public Text LogText;
+    public InputField ipInputField;
+    string networkStateText;
+    string LogTextContent = "";
+    string ip = "172.16.0.102";
+
+
+    TCPClient client;
+
+    private void Awake() {
+        MainThreadHandler.Init();
+    }
+
+    private void Start() {
+        InitClient();
+    }
+
+    void InitClient() {
+        if (client != null) {
+            // close
+        }
+        client = new TCPClient();
+        client.OnNetworkStateChanged += OnNetworkStateChanged;
+
+        IPAddress ipAddress = IPAddress.Parse(ip);
+        Debug.Log(ip + " => " + ipAddress);
+        client.Connect(ipAddress, 9000, () => {
+            AppendLog("连接成功");
+            MainThreadHandler.Invoke(() => {
+                StartLogin();
+            });
+        });
+    }
+
+    void OnNetworkStateChanged(NetworkState state) {
+        networkStateText = "网络状态改变" + state;
+        if (state == NetworkState.Error || state == NetworkState.Closed) {
+            client = null;
+        }
+        MainThreadHandler.Invoke(() => {
+            UpdateUI();
+        });
+    }
+
+    private void UpdateUI() {
+        networkState.text = networkStateText;
+        LogText.text = LogTextContent;
+    }
+
+    public void StartLogin() {
+        LogTextContent = "";
+
+        if (client == null) {
+            return;
+        }
+        client.Request("Example.Login", "hello".StringToBytes(), (r) => {
+            var msg = r.BytesToStringUTF8();
+            Debug.Log("收到: " + msg);
+            client.Request("Example.Test", r, (rr) => { });
+
+            MainThreadHandler.Invoke(() => {
+                AppendLog(msg);
+            });
+        });
+    }
+
+    void AppendLog(object msg) {
+        LogTextContent += msg != null ? msg.ToString() : "" + "\n";
+        MainThreadHandler.Invoke(() => {
+            UpdateUI();
+        });
+    }
+
+    public void ChangeIP() {
+        ip = ipInputField.text;
+        InitClient();
+    }
+}
---
+++ Assets/Scripts/MainThreadHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainThreadHandler : MonoBehaviour {
+    static MainThreadHandler _Instance;
+    static MainThreadHandler Instance {
+        get {
+            if (_Instance == null) {
+                _Instance = new GameObject("MainThreadHandler").AddComponent<MainThreadHandler>();
+                DontDestroyOnLoad(_Instance.gameObject);
+            }
+            return _Instance;
+        }
+    }
+    List<Action> actions = new List<Action>();
+    public static void Init() {
+        Instance.actions.Clear();
+        Invoke(() => { });
+    }
+    public static void Invoke(Action callback) {
+        lock(Instance.actions) {
+            Instance.actions.Add(callback);
+        }
+    }
+
+    private void Update() {
+        List<Action> invokes = new List<Action>();
+        lock (Instance.actions) {
+            invokes.AddRange(actions.ToArray());
+            actions.Clear();
+        }
+
+        foreach (var item in actions) {
+            try {
+                item.Invoke();
+            } catch (Exception e) {
+                Debug.LogError(e);
+            }
+        }
+    }
+}
